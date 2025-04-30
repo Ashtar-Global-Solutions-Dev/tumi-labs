dbb822c0707012d0fab6e0c96d87fccd0a0189b8
Aliyan Hussain <hussainaliyan531@gmail.com>
2025-04-30 18:12:04 +0500

    framer website injected using iframe

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div style={{ width: "100%", height: "100vh" }}>
+      <iframe
+        src="https://tumilabss.framer.website/"
+        style={{ width: "100%", height: "100%", border: "none" }}
+        allowFullScreen
+        scrolling="no"
+      />
+    </div>
+  );
+}
